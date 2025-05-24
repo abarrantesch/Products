5fd222f02c6c94f023a2a3a0acd6929d544b1a13
ajosu <ajosu@DESKTOP-8A3KP44>
2025-05-24 09:38:20 -0600

    Si

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        //Interfaz de usuario, menu
+        Console.WriteLine("Bienvenido al gestor de productos Dummy");
+        Console.WriteLine("Digite la opcion deseada:");
+        Console.WriteLine("1. Agregar un producto");
+        Console.WriteLine("2. Consultar listado de productos");
+        Console.WriteLine("3. Salir");
+
+        var option = Int32.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("Digite la informacion del producto a continuacion");
+                break;
+            case 2:
+                Console.WriteLine("Listado de productos existentes:");
+                break;
+            case 3:
+                Console.WriteLine("Saliendo del programa...");
+                break;
+            default:
+                Console.WriteLine("Opcion no valida, por favor intente de nuevo.");
+                break;
+        }
+
+    }
+}
