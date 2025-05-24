ead545a0f4beb41aa564e85a625c8c514c1276f3
ajosu <ajosu@DESKTOP-8A3KP44>
2025-05-24 11:21:25 -0600

    .

--- BusinessLogic/ProductManager.cs
+++ BusinessLogic/ProductManager.cs
@@ -1,7 +1,9 @@
-﻿using Entities;
+﻿using DataPersistence;
+using Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,28 @@ namespace BusinessLogic
 {
     public class ProductManager
     {
-        public void AddProduct()
+        public void AddProduct(Product product)
         {
+            //Aplicar validaciones
+            if(string.IsNullOrEmpty(product.Name))
+            {
+                throw new Exception("El nombre no puede estar vacio");
+            }
 
+            if (Product.Price < 1)
+            {
+                throw new Exception("El precio debe ser mayor que 0");    
+            }
+
+            if Product.Stock < 0)
+            {
+                throw new Exception("El stock debe ser mayor que 0");
+            }
+
+            //Si pasa las validaciones, guardar el producto en un txt
+
+            var fm=new FileManager();
+            fm.SaveProduct(product);
         }
         public List<Product> GetProducts()
         {
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,32 +1,62 @@
-﻿public class Program
+﻿using Entities;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        
-        //Interfaz de usuario, menu
-        Console.WriteLine("Bienvenido al gestor de productos Dummy");
-        Console.WriteLine("Digite la opcion deseada:");
-        Console.WriteLine("1. Agregar un producto");
-        Console.WriteLine("2. Consultar listado de productos");
-        Console.WriteLine("3. Salir");
-
-        var option = Int32.Parse(Console.ReadLine());
-
-        switch (option)
+        int option;
+        do
         {
-            case 1:
-                Console.WriteLine("Digite la informacion del producto a continuacion");
-                break;
-            case 2:
-                Console.WriteLine("Listado de productos existentes:");
-                break;
-            case 3:
-                Console.WriteLine("Saliendo del programa...");
-                break;
-            default:
-                Console.WriteLine("Opcion no valida, por favor intente de nuevo.");
-                break;
-        }
+            //Interfaz de usuario, menu
+            Console.WriteLine("Bienvenido al gestor de productos Dummy");
+            Console.WriteLine("Digite la opcion deseada:");
+            Console.WriteLine("1. Agregar un producto");
+            Console.WriteLine("2. Consultar listado de productos");
+            Console.WriteLine("3. Salir");
+
+            option = Int32.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Digite la informacion del producto a continuacion");
+                    Console.WriteLine("Indique el ID:"); 
+                    var id = Int32.Parse(Console.ReadLine()); //captura ID
+                    Console.WriteLine("Indique el nombre:");
+                    var name = Console.ReadLine(); //captura Nombre
+                    Console.WriteLine("Indique la cantidad disponible:");
+                    var stock = Int32.Parse(Console.ReadLine()); //captura Stock
+                    Console.WriteLine("Indique el precio:");
+                    var price = Double.Parse(Console.ReadLine()); //captura Precio
+
+                    var myProduct = new Product() { Id = id, Name = name, Stock = stock, Price = price }; //instancia de la clase producto
+
+                    var pm=new ProductManager() { Id = id, Name = name, Stock = stock, Price = price } //COMPLETAR
+
+                        try
+                    {
+                        Pm.addProduct(myProduct);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+
+                    break;
+                case 2:
+                    Console.WriteLine("Listado de productos existentes:");
+                    break;
+                case 3:
+                    Console.WriteLine("Saliendo del programa...");
+                    break;
+                default:
+                    Console.WriteLine("Opcion no valida, por favor intente de nuevo.");
+                    break;
+            }
+        } while (option != 3); //do while para que se ejecute al menos una vez y se repita hasta que el usuario decida salir.
 
     }
 }
