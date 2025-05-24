33054c9f4264dce14d5cbb0704fd66b28caa9054
ajosu <ajosu@DESKTOP-8A3KP44>
2025-05-24 09:38:09 -0600

    Estructura General del programa

---
+++ DataPersistence/FileManager.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPersistence
+{
+    public class FileManager
+    {
+        public List<Product> ReadProducts()
+        {   //leer la lista de productos desde un txt y cargarla en una lista.
+            var products = new List<Product>();
+            return products;
+        }
+
+        public void SaveProduct(Product product)
+        {
+            // guardar el producto en un txt
+        }
+    }
+}
